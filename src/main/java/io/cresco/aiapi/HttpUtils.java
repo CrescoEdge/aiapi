04c33f7a4a6a020befc538e82b07458e788e063d
Cody Bumgardner <cody@uky.edu>
2024-03-06 15:32:40 -0500

    adding adapter

--- src/main/java/io/cresco/aiapi/HttpUtils.java
+++ src/main/java/io/cresco/aiapi/HttpUtils.java
@@ -1,19 +1,31 @@
 package io.cresco.aiapi;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import io.cresco.library.messaging.MsgEvent;
+import io.cresco.library.plugin.PluginBuilder;
+import io.cresco.library.utilities.CLogger;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.api.Request;
 import org.eclipse.jetty.client.util.StringContentProvider;
 import org.eclipse.jetty.http.HttpHeader;
 
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Map;
 
 public class HttpUtils {
 
+    private PluginBuilder plugin;
     private final Gson gson;
-    public HttpUtils() {
+    private CLogger logger;
+
+    Type typeOfListMap = new TypeToken<List<Map<String,String>>>(){}.getType();
+
+    public HttpUtils(PluginBuilder plugin) {
+        this.plugin = plugin;
+        logger = plugin.getLogger(PluginExecutor.class.getName(),CLogger.Level.Info);
         gson = new Gson();
     }
 
@@ -33,22 +45,25 @@ public class HttpUtils {
             String requestString = null;
             if(adapterId != null) {
                 requestString = "{\"inputs\":\"" + inputString + "\",\"parameters\":{\"max_new_tokens\":" + maxTokens +
-                        ", \"adapter_id\":" + adapterId + ", \"adapter_source\":\"local\"}}";
+                        ", \"adapter_id\":\"" + adapterId + "\", \"adapter_source\":\"local\"}}";
             } else {
                 requestString = "{\"inputs\":\"" + inputString + "\",\"parameters\":{\"max_new_tokens\":" + maxTokens + "}}";
             }
+            logger.error("requesting String: " + requestString);
 
             HttpClient client = new HttpClient();
             client.setFollowRedirects(false);
             client.start();
-            
+
             Request request = client.POST(url);
             request.header(HttpHeader.CONTENT_TYPE, "application/json");
 
             request.content(new StringContentProvider(requestString,"utf-8"));
             ContentResponse response = request.send();
-
-            String llmResponse = String.valueOf(gson.fromJson(response.getContentAsString(), Map.class).get("generated_text"));
+            String contentString = response.getContentAsString();
+            logger.error("content: " + contentString);
+            List<Map<String,String>> lmresponse = gson.fromJson(contentString, typeOfListMap);
+            String llmResponse = String.valueOf(lmresponse.get(0).get("generated_text"));
             int responseStatus = response.getStatus();
 
             client.stop();
